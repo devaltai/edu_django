596df3650737567eebd8b1d4a75b61ef1fb6c799
devaltai <yalatov@gmail.com>
2022-01-08 01:10:50 +0700

    redactor: 21urok

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('', index)
+    path('', index, name='home'),
+    path('category/<int:category_id>/', get_category, name='category'),
+    path('news/<int:news_id>/', view_news, name='view_news'),
 ]
