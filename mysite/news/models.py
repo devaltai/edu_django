7791dd769ba92433939f96769d71046d00f83174
devaltai <yalatov@gmail.com>
2022-01-18 14:21:29 +0700

    redactor class Create Detail List

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -9,10 +9,10 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, verbose_name='Фото')
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
 
     def get_absolute_url(self):
-        return reverse('view_news', kwargs={"news_id": self.pk})
+        return reverse('view_news', kwargs={"pk": self.pk})
 
     def __str__(self):
         return self.title
