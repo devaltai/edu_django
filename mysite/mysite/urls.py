59ffcb9da3b7c9dc83dc8c3abb1012902c05fdc3
Евгений Ялатов <devaltai@MacBook-Air-Evgenij.local>
2022-01-30 22:58:46 +0700

    captcha

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,12 +20,15 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('captcha/', include('captcha.urls')),
     path('', include('news.urls')),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
+
     urlpatterns = [
-        path('__debug__/', include('debug_toolbar.urls')),
-    ]+urlpatterns
+                      path('__debug__/', include('debug_toolbar.urls')),
+                  ] + urlpatterns
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
